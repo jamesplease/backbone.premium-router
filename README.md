4f1afcfd892e98eb88875651957723e27c7c4749
Jmeas <jellyes2@gmail.com>
2014-11-07 21:15:48 +0100

    Initial work

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-backbone.state-router
-=====================
+# backbone.state-router
 
-A Backbone Router that can manage the state of your application.
+A Router that manages the state of your application.
+
+# About
+
+Backbone's Router, like many other Backbone objects, is much too simple for anything
+other than the simplest of applications. But the Router is worse off than other objects,
+because it's hardcoded to function in basically only way: setting up the initial state of your
+application.
+
+The success of Angular's UI-Router and Ember's Router have demonstrated that routers can, and
+should, do more than that. This Router adopts the philosophies of those two routers by providing
+a simple interface for managing your app's state.
+
+### Dependencies
+
+- `window.Promise` must exist, and it must be A+ compliant.
+- Underscore
+- Backbone
+
+### Getting Started
---
+++ gruntfile.js
@@ -0,0 +1,160 @@
+module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    meta: {
+      version: '<%= pkg.version %>',
+      banner: '// Backbone.StateRouter v<%= meta.version %>\n'
+    },
+
+    preprocess: {
+      router: {
+        src: 'src/wrapper.js',
+        dest: 'build/backbone.state-router.js'
+      }
+    },
+
+    template: {
+      options: {
+        data: {
+          version: '<%= meta.version %>'
+        }
+      },
+      router: {
+        src: '<%= preprocess.router.dest %>',
+        dest: '<%= preprocess.router.dest %>'
+      }
+    },
+
+    concat: {
+      options: {
+        banner: '<%= meta.banner %>'
+      },
+      router: {
+        src: '<%= preprocess.router.dest %>',
+        dest: '<%= preprocess.router.dest %>'
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: '<%= meta.banner %>'
+      },
+      router: {
+        src: '<%= preprocess.router.dest %>',
+        dest: 'build/backbone.state-router.min.js',
+        options: {
+          sourceMap: true
+        }
+      }
+    },
+
+    jshint: {
+      router: {
+        options: {
+          jshintrc: '.jshintrc'
+        },
+        src: ['src/*.js', '!src/wrapper.js']
+      },
+      // tests: {
+      //   options: {
+      //     jshintrc: 'test/.jshintrc'
+      //   },
+      //   src: ['test/spec/*.js']
+      // }
+    },
+
+    mochaTest: {
+      spec: {
+        options: {
+          require: 'test/setup/node.js',
+          reporter: 'dot',
+          clearRequireCache: true,
+          mocha: require('mocha')
+        },
+        src: [
+          'test/setup/helpers.js',
+          'test/spec/*.js'
+        ]
+      }
+    },
+
+    env: {
+      coverage: {
+        APP_DIR_FOR_CODE_COVERAGE: '../../coverage/src/'
+      }
+    },
+
+    instrument: {
+      files: 'src/*.js',
+      options: {
+        lazy: true,
+        basePath: 'coverage'
+      }
+    },
+
+    storeCoverage: {
+      options: {
+        dir: 'coverage'
+      }
+    },
+
+    makeReport: {
+      src: 'coverage/**/*.json',
+      options: {
+        type: 'lcov',
+        dir: 'coverage',
+        print: 'detail'
+      }
+    },
+
+    coveralls: {
+      options: {
+        src: 'coverage/lcov.info',
+        force: false
+      },
+      default: {
+        src: 'coverage/lcov.info'
+      }
+    },
+
+    watch: {
+      tests: {
+        options: {
+          spawn: false
+        },
+        files: ['src/**/*.js', 'test/spec/**/*.js'],
+        tasks: ['test']
+      }
+    }
+  });
+
+  grunt.registerTask('test', 'Test the library', [
+    'jshint',
+    'mochaTest'
+  ]);
+
+  grunt.registerTask('coverage', 'Generate coverage report for the library', [
+    'env:coverage',
+    'instrument',
+    'mochaTest',
+    'storeCoverage',
+    'makeReport',
+    'coveralls'
+  ]);
+
+  grunt.registerTask('build', 'Build the library', [
+    'test',
+    'preprocess:router',
+    'template',
+    'concat',
+    'uglify'
+  ]);
+
+  grunt.registerTask('default', 'An alias of test', [
+    'test'
+  ]);
+};
